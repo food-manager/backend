8832e861ae7ef1511c39a338a6cb45cb0b77c3b5
Darko Vucetic <91341238+d-vucetic@users.noreply.github.com>
2022-07-12 15:22:34 +0200

    1 jwt auth (#29)
    * added jwt request filter

--- src/main/java/com/g1dra/foodmanager/contollers/FoodController.java
+++ src/main/java/com/g1dra/foodmanager/contollers/FoodController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/foods")
+@RequestMapping("/api/foods")
 public class FoodController {
     private final FoodRepository foodRepository;
 
