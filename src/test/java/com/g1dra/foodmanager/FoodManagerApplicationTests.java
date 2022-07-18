2d44e5064658fb6f709797ed903d35a8fdf7dd2f
Milica Perović Mršović <98592596+mperovicmrsovic@users.noreply.github.com>
2022-07-18 13:29:03 +0200

    Added setup for H2 (#34)
    Added setup for H2

--- src/test/java/com/g1dra/foodmanager/FoodManagerApplicationTests.java
+++ src/test/java/com/g1dra/foodmanager/FoodManagerApplicationTests.java
@@ -2,10 +2,11 @@ package com.g1dra.foodmanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-public
-class FoodManagerApplicationTests {
+@ActiveProfiles("test")
+public class FoodManagerApplicationTests {
 
     @Test
     void contextLoads() {
